847a8d73ba50d9a49ac8c6ab6dde4e4d11aa9c29
Vijay Ethuraj <evijay77@gmail.com>
2021-01-07 21:53:14 -0800

    register and login

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/vijaysoul/devcom/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
+	conn, err = connectDB()
+	if err != nil {
+		return
+	}
+
 	router := gin.Default()
+
+	router.Use(dbMiddleware(*conn))
+
 	usersGroup := router.Group("users")
 	{
-		usersGroup.POST("register", routes.UsersRegister)
+		usersGroup.POST("register", dbMiddleware, routes.UsersRegister)
 	}
 
 	router.Run(":3000")
+}
+
+func connectDB() (c *pgx.Conn, erro error) {
+	conn, err := pgx.Connect(context.Background(), "postgresql://postgres:vijay@localhost:5432/devcom")
+	if err != nil {
+		fmt.Println(("Error connecting to DB"))
+		fmt.Println(err.Error())
+	}
+	_ = conn.Ping(context.Background())
+	return conn, err
+}
+
+func dbMiddleware(conn, pgx.Conn) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Set("db", conn)
+		c.Next()
+	}
 }
--- routes/users.go
+++ routes/users.go
@@ -1,10 +1,38 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
+	"routes/models"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v4"
 )
 
 func UsersRegister(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"user_id": "someid"})
+	user := models.User{}
+	err := c.ShouldBindJSON(&user)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	db, _ := c.Get("db")
+	conn := db.(pgx.Conn)
+	err = user.Register(&conn)
+	if err != nil {
+		fmt.Println("error in user.Register()")
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+	}
+
+	token, err := user.GetAuthToken()
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"token": token,
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"user_id": user.ID,
+	})
 }
